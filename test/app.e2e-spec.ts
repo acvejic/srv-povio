5ef857ea7b12bf4a3065646dea4da4c9571adfb7
acvejic <cvejica@gmail.com>
2023-09-22 00:27:03 +0200

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
-console.log({
-  host: configService.get('DATABASE_HOST', 'localhost'),
-  port: configService.get<number>('DATABASE_PORT', 5432),
-  username: configService.get('DATABASE_USER', 'postgres'),
-  password: configService.get('DATABASE_PASS', 'postgres'),
-  database: configService.get<string>('DATABASE_SCHEMA', 'app'),
-});
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,6 +13,17 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+  // Retrieve the ConfigService from the testing module's container
+  const configService = moduleFixture.get<ConfigService>(ConfigService);
+
+  // Log the database configuration
+  console.log({
+    host: configService.get('DATABASE_HOST', 'localhost'),
+    port: configService.get<number>('DATABASE_PORT', 5432),
+    username: configService.get('DATABASE_USER', 'postgres'),
+    password: configService.get('DATABASE_PASS', 'postgres'),
+    database: configService.get<string>('DATABASE_SCHEMA', 'app'),
+  });    
     await app.init();
   });
 
