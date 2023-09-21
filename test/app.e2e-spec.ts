95c7dd87e9aa8ec86e6db8ed7452522188c0e1e0
acvejic <cvejica@gmail.com>
2023-09-22 00:23:41 +0200

    console.log

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,14 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+console.log({
+  host: configService.get('DATABASE_HOST', 'localhost'),
+  port: configService.get<number>('DATABASE_PORT', 5432),
+  username: configService.get('DATABASE_USER', 'postgres'),
+  password: configService.get('DATABASE_PASS', 'postgres'),
+  database: configService.get<string>('DATABASE_SCHEMA', 'app'),
+});
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
